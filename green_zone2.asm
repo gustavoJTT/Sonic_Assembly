3fb790557b5f250fbb8a4290dd5f06ebfde63a65
gustavoJTT <gustavojosetata@gmail.com>
2025-01-01 16:43:42 -0300

    tudo pronto, eu acho

--- green_zone2.asm
+++ green_zone2.asm
@@ -1,55 +1,80 @@
 .text
 
 main:
-	lui $8 0x1001
-	ori $9 $0 0x1D009F #azul ceu
-	ori $10 $0 0x0082E0 #azul mar
-	ori $11 $0 0x250000 #marrom escuro
-	ori $12 $0 0x6A1B00 #marrom claro
-	ori $13 $0 0x62FF36 #verde claro
-	ori $14 $0 0x00AD2B #verde escuro
-	
-	#addi $25 $0 8192 #mÃ¡ximo da tela
-	
-	addi $25 $0 3328
-	
+  lui $8 0x1001
+  ori $9 $0 0x1D009F #azul ceu
+  ori $10 $0 0x0082E0 #azul mar
+  ori $11 $0 0x250000 #marrom escuro
+  ori $12 $0 0x6A1B00 #marrom claro
+  ori $13 $0 0x62FF36 #verde claro
+  ori $14 $0 0x00AD2B #verde esuro
+  ori $15 $0 0xFFFFFF #branco
+
+  #addi $25 $0 8192 #mÃ¡ximo da tela
+
+  addi $25 $0 3303
+  addi $24 $0 2655
+  addi $23 $0 2320
+  addi $22 $0 2132
+  addi $21 $0 1338
+  addi $20 $0 908
+
 ceu:
-	beq $25 $0 montanhas_prep
-	
-	sw $9 0($8)
-	
-	addi $8 $8 4
-	addi $25 $25 -1
-	j ceu
+  beq $25 $0 montanhas_prep
 
-montanhas_prep: addi $22 $0 11
+  sw $9 0($8)
 
-montanha1_prep: beq  $22 $0 mar_prep
-                addi $22 $22 -1
-                addi $24 $0 7 #$24 > altura montanha 
-                addi $23 $0 1
+  addi $8 $8 4
+  addi $25 $25 -1
+  beq $25 $24 nuvem
+  beq $25 $23 nuvem
+  beq $25 $22 nuvem 
+  beq $25 $21 nuvem 
+  beq $25 $20 nuvem 
+  j ceu
 
-montanha1: jal criar_montanha
+nuvem:
+  jal criar_nuvem
+  j ceu
 
-montanha2_prep: addi $24 $0 5
-                addi $23 $0 1 
- 
-montanha2: jal criar_montanha
-           #sw $10 4($8)
-          # addi $8 $8 4
-           j montanha1_prep
+montanhas_prep:
+  addi $22 $0 10
+
+montanha1_prep:
+  beq  $22 $0 mar_prep
+  addi $22 $22 -1
+  addi $24 $0 7 #$24 > altura montanha 
+  addi $23 $0 1
+
+montanha1:
+  jal criar_montanha
+
+montanha2_prep:
+  addi $24 $0 5
+  addi $23 $0 1 
+
+montanha2:
+  jal criar_montanha
+
+  addi $8 $8 4
+  sw $10 0($8)
+  j montanha1_prep
 
 mar_prep: 
-	addi $25 $0 2006
-	
+  addi $25 $0 2262
+  addi $8 $8 -8
+  addi $23 $0 260
+
 mar:   
-	beq $25 $0 grama_prep
+  beq $25 $0 grama_prep
 	
-	sw $10 0($8)
-	
-	addi $8 $8 4
-	addi $25 $25 -1
-	j mar 
+  sw $10 0($8)
+  jal ondas_verif
+
+  addi $8 $8 4
+  addi $25 $25 -1
+  addi $23 $23 -1
+  j mar 
 
 
 
@@ -58,47 +83,41 @@ grama_prep:
   addi $23 $0 10
 
 grama: 
-	beq $25 $0 solo_prep
-	jal flor_verif
+  beq $25 $0 solo_prep
+  jal flor_verif
   sw $14 0($8)
   sw $14 4($8)
   sw $13 8($8)
-       
+
   addi $8 $8 12
   addi $25 $25 -1
   addi $23 $23 -1
   j grama
 
-
-
-
 solo_prep: 
-	addi $25 $25 9
+  addi $25 $25 8
   addi $8 $8 -512
-           
+
 
 solo_laco1:
-	beq $25 $0 fim
+  beq $25 $0 fim
   addi $24 $0 64
-           
+
   addi $8 $8 512
   addi $25 $25 -1
 
 solo_laco2:
-	beq $24 $0 solo_laco1
-           
+  beq $24 $0 solo_laco1
+
   jal solo
-           
+
   addi $24 $24 -1
   addi $8 $8 8
-  j solo_laco2
+j solo_laco2
 
 
-
-
-	
 fim:
-	addi $2 $0 10
+  addi $2 $0 10
   syscall
 
 #----------------------func-----------------------
@@ -106,101 +125,159 @@ fim:
 #função solo para prencher o solo de jeito alternado
 
 solo: 
-	lw $23 -4($8)
-	addi $22 $0 64
+  lw $23 -4($8)
+  addi $22 $0 64
   beq $22 $24 escolha_solo
-      
+
   beq $23 $12 marr_esc
   j marr_claro
 
-	
+
 escolha_solo: 
-	bne $23 $12 marr_esc 
-    
+  bne $23 $12 marr_esc 
+
 marr_claro:
-	sw $12 0($8)
+  sw $12 0($8)
   sw $12 4($8)
   sw $12 512($8)
   sw $12 516($8)
   j fim_func_solo
-                   
+
 marr_esc:
-	sw $11 0($8)
+  sw $11 0($8)
   sw $11 4($8)
   sw $11 512($8)
   sw $11 516($8)
   j fim_func_solo
 
 fim_func_solo:
-	jr $31
-	
-	
+  jr $31
+
+
 # função criar_montanha para criar as montanhas com as medidadas dadas
+criar_montanha:
+  addi $21 $0 0
+  addi $20 $0 1
+  addi $19 $0 0
+  addi $24 $24 2
+  addi $8 $8 512
 
-criar_montanha: addi $21 $0 0
-                addi $20 $0 1
-                addi $19 $0 0
-                addi $24 $24 2
-                addi $8 $8 512
+montanha_subindo:
+  beq $24 $23 montanha_descendo_prep
 
-montanha_subindo: beq $24 $23 montanha_descendo_prep
+montanha_subindo2:
+  beq $21 $23 fim_montanha_subindo
+  sw $11 -512($8)
+  addi $21 $21 1 
+  addi $8 $8 -512
+  addi $19 $19 512
+  j montanha_subindo2
 
-montanha_subindo2: beq $21 $23 fim_montanha_subindo
-                   sw $11 -512($8)
-                   addi $21 $21 1 
-                   addi $8 $8 -512
-                   addi $19 $19 512
-                   j montanha_subindo2
-                   
-fim_montanha_subindo: addi $23 $23 2
-                      addi $21 $0 0
-                      add $8 $8 $19
-                      addi $19 $0 0
-                      addi $8 $8 4
-                      j montanha_subindo
+fim_montanha_subindo:
+  addi $23 $23 2
+  addi $21 $0 0
+  add $8 $8 $19
+  addi $19 $0 0
+  addi $8 $8 4
+  j montanha_subindo
 
-montanha_descendo_prep: addi $23 $23 -2
+montanha_descendo_prep:
+  addi $23 $23 -2
 
-montanha_descendo: beq $23 $20 fim_func_montanhas
-                   addi $23 $23 -2
-                   
- 
-montanha_descendo2: beq $21 $23 fim_montanha_descendo
-                   sw $11 -512($8)
-                   addi $21 $21 1 
-                   addi $8 $8 -512
-                   addi $19 $19 512
-                   j montanha_descendo2
-                   
-fim_montanha_descendo: 
-                      addi $21 $0 0
-                      add $8 $8 $19
-                      addi $19 $0 0
-                      addi $8 $8 4
-                      j montanha_descendo                  
+montanha_descendo:
+  beq $23 $20 fim_func_montanhas
+  addi $23 $23 -2
 
 
-fim_func_montanhas: addi $8 $8 -512
-                    jr $31
-                    
-                    
-flor_verif:
-	beq $23 $0 flor_draw
+montanha_descendo2:
+  beq $21 $23 fim_montanha_descendo
+  sw $11 -512($8)
+  addi $21 $21 1 
+  addi $8 $8 -512
+  addi $19 $19 512
+  j montanha_descendo2
+
+fim_montanha_descendo:
+  addi $21 $0 0
+  add $8 $8 $19
+  addi $19 $0 0
+  addi $8 $8 4
+  j montanha_descendo                  
+
+
+fim_func_montanhas: 
+  addi $8 $8 -512
+  jr $31
+  
+  
+ondas_verif:
+	beq $23 $0 ondas_draw
 	
 	jr $31
 	
-flor_draw:
+ondas_draw:
 	addi $9 $0 0
-	ori $9 $0 0x442305 #marrom meio flor
-	ori $10 $0 0xF7D10F #cor flor
-	
-	sw $12 -512($8)
-	sw $12 -1024($8)
-	sw $10 -1536($8)
-	sw $10 -2052($8)
-	sw $10 -2044($8)
-	sw $9 -2048($8)
-	sw $10 -2560($8)
-	
-	addi $23 $0 10
+  ori $9 $0 0x87B8BD #espuma das ondas
+  
+  sw $9 -1024($8)
+  sw $9 -1040($8)
+  sw $9 -516($8)
+  sw $9 -520($8)
+  sw $9 -512($8)
+  sw $9 -524($8)
+  sw $9 -528($8)
+  sw $9 -532($8)
+
+  addi $23 $0 50
 	jr $31
+
+
+flor_verif:
+  beq $23 $0 flor_draw
+
+  jr $31
+
+flor_draw:
+  addi $9 $0 0
+  ori $9 $0 0x442305 #marrom meio flor
+  addi $10 $0 0
+  ori $10 $0 0xF7D10F #cor flor
+
+  sw $12 -512($8)
+  sw $12 -1024($8)
+  sw $10 -1536($8)
+  sw $10 -2052($8)
+  sw $10 -2044($8)
+  sw $9 -2048($8)
+  sw $10 -2560($8)
+
+  addi $23 $0 10
+	jr $31
+
+
+#função criar_nuvem 
+criar_nuvem: 
+  sw $15 -516($8)
+  sw $15 -520($8)
+  sw $15 -1028($8)
+  sw $15 0($8)
+  sw $15 -512($8)
+  sw $15 -1024($8)
+  sw $15 4($8)
+  sw $15 -508($8)
+  sw $15 -1020($8)
+  sw $15 -1532($8)
+  sw $15 8($8)
+  sw $15 -504($8)
+  sw $15 -1016($8)
+  sw $15 -1528($8)
+  sw $15 12($8)
+  sw $15 -1012($8)
+  sw $15 -1008($8)
+  sw $15 -1004($8)
+  sw $15 -500($8)
+  sw $15 -496($8)
+  sw $15 -492($8)
+  sw $15 16($8)
+  addi $8 $8 20
+  jr $31
