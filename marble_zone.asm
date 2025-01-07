890c56b299217cb222446e186806071504d051e7
gustavoJTT <gustavojosetata@gmail.com>
2025-01-07 16:36:02 -0300

    parede marble zone

--- marble_zone.asm
+++ marble_zone.asm
@@ -6,41 +6,26 @@ main:
   ori $10 $0 0x673F84 #roxo linha solo
   ori $11 $0 0x1F001F #roxo escuro fundo
   ori $12 $0 0x48214F #roxo linha fundo
-  ori $13 $0 0
+  ori $13 $0 0xffffff #branco
   ori $14 $0 0
   ori $15 $0 0xFFFFFF #branco
   
 fundo_prep:
 	addi $25 $0 8192
+	addi $24 $0 1024
+	addi $23 $0 13
 	
 fundo_draw:
-	beq $25 $0 linha_prep
+	beq $25 $0 fim
 	
 	sw $11 0($8)
+	jal teste_height
 	
 	addi $8 $8 4
 	addi $25 $25 -1
-	j fundo_draw
-	
-linha_prep:
-	addi $8 $0 0
-	lui $8 0x1001
-	addi $25 $0 16
-	
-	addi $24 $0 1024 #altura tijolo
-	addi $23 $0 75	#largura tijolo
-	
-linha_draw:
-	beq $25 $0 fim
-	
-	jal teste_height
-	jal teste_width
-	
-	addi $8 $8 4
 	addi $24 $24 -1
-	addi $23 $23 -1
-	j linha_draw
-		
+	j fundo_draw
+
 fim:
   addi $2 $0 10
   syscall
@@ -54,34 +39,42 @@ teste_height:
 
 height_draw:
 	addi $24 $0 128
-	add $22 $8 $0
+	addi $23 $23 2
+	add $31 $0 $20
 		
 height_draw_l1:
 	beq $24 $0 height_fim
 	
-	sw $12 0($22)
+	sw $12 0($8)
+	jal brick_test
 	
-	addi $22 $22 4
+	addi $8 $8 4
 	addi $24 $24 -1
+	addi $23 $23 -1
+	
 	j height_draw_l1
 	
 height_fim:
 	addi $24 $0 1024
+	addi $23 $0 15
+	
+	jr $20
+	
+	
+brick_test:
+	beq $23 $0 brick_draw
+	
 	jr $31
 	
+brick_draw:
+	sw $13 -512($8)
+	sw $13 -1024($8)
+	sw $13 -1536($8)
+	sw $13 -2048($8)
+	sw $13 -2560($8)
+	sw $13 -3072($8)
+	sw $13 -3584($8)
+	sw $13 -4096($8)
 	
-	
-	
-	
-teste_width:
-	beq $23 $0 width_draw
-	
-	jr $31
-	
-width_draw:
-	sw $12 -512($8)
-	sw $12 -1024($8)
-	sw $12 -1536($8)
-	
-	addi $23 $0 75
+	addi $23 $0 15
 	jr $31
