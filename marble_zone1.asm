8612b287b1ef6a484f90ab112079f7e03bd4c6ec
gustavoJTT <gustavojosetata@gmail.com>
2025-01-09 21:20:35 -0300

    castelo

--- marble_zone1.asm
+++ marble_zone1.asm
@@ -57,16 +57,16 @@ cespugli_outro_strato: addi $8 $8 1536
 castelo: 
 
 grama_marble_zone_prep: addi $8 $8 1520
+												add $20 $8 $0
+												add $19 $8 $0
                         ori $9 $0 0x00AD2B #verde escuro
                         ori $10 $0 0x62FF36 #verde claro
-                        ori $11 $0 0xfff
                         addi $25 $0 384
                         
 
 grama_marble_zone: beq $25 $0 solo_marble_zone1
                    sw $10 0($8)
                    sw $9 4($8)
-                   #sw $11 0($8) KHBFJQBFQfviwyqg fazer dps daqui o castelo
                    
                    addi $8 $8 8
                    addi $25 $25 -2
@@ -76,7 +76,41 @@ solo_marble_zone1: addi $25 $25 3712
                    addi $24 $25 -256
                    addi $23 $25 -34
                    
-                   jal solo_marble_zone_func
+                   jal solo_marble_zone_func                 
+
+castelo_base_luz_main:
+	jal castelo_luz_prep_func
+
+castelo_base_sombra_main:
+	jal castelo_sombra_prep_func
+	
+castelo_coluna_luz_main:
+	addi $20 $19 280
+	jal castelo_coluna_luz_prep_func
+	
+castelo_coluna_sombra_main:
+	addi $20 $19 280
+	jal castelo_coluna_sombra_prep_func
+
+
+castelo_base_luz_main_2:
+	addi $20 $19 120
+	jal castelo_luz_prep_func
+
+castelo_base_sombra_main_2:
+	jal castelo_sombra_prep_func
+	
+castelo_coluna_luz_main_2:
+	addi $20 $19 400
+	jal castelo_coluna_luz_prep_func
+	
+castelo_coluna_sombra_main_2:
+	addi $20 $19 400
+	jal castelo_coluna_sombra_prep_func
+	
+castelo_topo_main:
+	addi $20 $19 280
+	jal castelo_topo_prep_func
 
 fim: addi $2 $0 10
      syscall
@@ -379,3 +413,261 @@ buraco_func_laco: beq $25 $23 fim_func_buraco
 fim_func_buraco: addi $23 $23 -100
                  jr $31
 
+
+
+	#func castelo
+	
+voltar:
+	jr $31
+	
+voltar_alternativo:
+	jr $18
+	
+	#base clara
+castelo_luz_prep_func:
+	addi $20 $20 280 #final da grama + dist para dps do buraco
+	addi $25 $0 3
+	
+	ori $9 $0 0xA9DFAF #verde claro luz
+	ori $10 $0 0x72987B #verde escuro luz
+	
+	sw $9 -516($20)
+	sw $9 -512($20)
+	sw $9 -508($20)
+	sw $9 -504($20)
+	sw $9 -500($20)
+	sw $9 -496($20)
+	sw $9 -492($20)
+	sw $9 -488($20)
+	
+	sw $9 -1024($20)
+	sw $9 -1020($20)
+	sw $9 -1016($20)
+	sw $10 -1012($20)
+	sw $9 -1008($20)
+	sw $9 -1004($20)
+	sw $9 -1000($20)
+
+castelo_base_luz_func:
+	beq $25 $0 voltar
+	
+	sw $9 1024($20)
+	sw $9 512($20)
+	sw $9 0($20)
+	
+	sw $10 1028($20)
+	sw $10 516($20)
+	sw $10 4($20)
+	
+	sw $9 1032($20)
+	sw $9 520($20)
+	sw $9 8($20)
+	
+	addi $20 $20 8
+	addi $25 $25 -1
+	j castelo_base_luz_func
+	
+	
+	#base escura
+castelo_sombra_prep_func:
+	addi $20 $20 4
+	addi $25 $0 3
+	
+	ori $9 $0 0x5D956D #verde claro sombra
+	ori $10 $0 0x0D6334 #verde escuro sombra
+	
+	sw $9 -484($20)
+	sw $9 -512($20)
+	sw $9 -508($20)
+	sw $9 -504($20)
+	sw $9 -500($20)
+	sw $9 -496($20)
+	sw $9 -492($20)
+	sw $9 -488($20)
+	
+	sw $9 -1024($20)
+	sw $9 -1020($20)
+	sw $9 -1016($20)
+	sw $10 -1012($20)
+	sw $9 -1008($20)
+	sw $9 -1004($20)
+	sw $9 -1000($20)
+
+castelo_base_sombra_func:
+	beq $25 $0 voltar
+	
+	sw $10 1024($20)
+	sw $10 512($20)
+	sw $10 0($20)
+	
+	sw $9 1028($20)
+	sw $9 516($20)
+	sw $9 4($20)
+	
+	sw $10 1032($20)
+	sw $10 520($20)
+	sw $10 8($20)
+	
+	addi $20 $20 8
+	addi $25 $25 -1
+	j castelo_base_sombra_func
+	
+	
+	#coluna clara
+castelo_coluna_luz_prep_func:
+	addi $25 $0 2
+	
+	ori $9 $0 0xA9DFAF #verde claro luz
+	ori $10 $0 0x72987B #verde escuro luz
+	
+castelo_coluna_luz_func:
+	beq $25 $0 voltar
+	
+	sw $9 -1536($20)
+	sw $9 -1532($20)
+	sw $10 -1528($20)
+	sw $10 -1524($20)
+	sw $10 -1520($20)
+	sw $9 -1516($20)
+	sw $9 -1512($20)
+	
+	sw $9 -2048($20)
+	sw $10 -2044($20)
+	sw $10 -2040($20)
+	sw $9 -2036($20)
+	sw $10 -2032($20)
+	sw $10 -2028($20)
+	sw $9 -2024($20)
+	
+	sw $9 -2560($20)
+	sw $10 -2556($20)
+	sw $10 -2552($20)
+	sw $9 -2548($20)
+	sw $10 -2544($20)
+	sw $10 -2540($20)
+	sw $9 -2536($20)
+	
+	sw $9 -3072($20)
+	sw $9 -3068($20)
+	sw $10 -3064($20)
+	sw $10 -3060($20)
+	sw $10 -3056($20)
+	sw $9 -3052($20)
+	sw $9 -3048($20)
+	
+	sw $9 -3584($20)
+	sw $9 -3580($20)
+	sw $9 -3576($20)
+	sw $10 -3572($20)
+	sw $9 -3568($20)
+	sw $9 -3564($20)
+	sw $9 -3560($20)
+	
+	addi $20 $20 -2560
+	addi $25 $25 -1
+	j castelo_coluna_luz_func
+	
+	
+castelo_coluna_sombra_prep_func:
+	addi $25 $0 2
+	
+	ori $9 $0 0x5D956D #verde claro sombra
+	ori $10 $0 0x0D6334 #verde escuro sombra
+	
+castelo_coluna_sombra_func:
+	beq $25 $0 voltar
+	
+	sw $9 -1508($20)
+	sw $9 -1504($20)
+	sw $10 -1500($20)
+	sw $10 -1496($20)
+	sw $10 -1492($20)
+	sw $9 -1488($20)
+	sw $9 -1484($20)
+	
+	sw $9 -2020($20)
+	sw $10 -2016($20)
+	sw $10 -2012($20)
+	sw $9 -2008($20)
+	sw $10 -2004($20)
+	sw $10 -2000($20)
+	sw $9 -1996($20)
+	
+	sw $9 -2532($20)
+	sw $10 -2528($20)
+	sw $10 -2524($20)
+	sw $9 -2520($20)
+	sw $10 -2516($20)
+	sw $10 -2512($20)
+	sw $9 -2508($20)
+	
+	sw $9 -3044($20)
+	sw $9 -3040($20)
+	sw $10 -3036($20)
+	sw $10 -3032($20)
+	sw $10 -3028($20)
+	sw $9 -3024($20)
+	sw $9 -3020($20)
+	
+	sw $9 -3556($20)
+	sw $9 -3552($20)
+	sw $9 -3548($20)
+	sw $10 -3544($20)
+	sw $9 -3540($20)
+	sw $9 -3536($20)
+	sw $9 -3532($20)
+	
+	addi $20 $20 -2560
+	addi $25 $25 -1
+	j castelo_coluna_sombra_func
+	
+	
+	#topo do castelo
+castelo_topo_prep_func:
+	addi $25 $0 52
+	
+	ori $9 $0 0xA7D4B1 #verde claro
+	ori $10 $0 0x89B996 #verde escuro
+	
+castelo_topo_base:
+	beq $25 $0 castelo_topo_lateral_prep
+	
+	sw $10 -6672($20)	#inicio do triagulo em cima
+	
+	addi $25 $25 -1
+	addi $20 $20 4
+	j castelo_topo_base
+	
+castelo_topo_lateral_prep:
+	addi $25 $0 3
+	addi $20 $19 280
+	
+	ori $9 $0 0xA7D4B1 #verde claro
+	
+castelo_topo_lateral:
+	beq $25 $0 castelo_topo_lateral_esquerda_prep
+	
+	sw $9 -6672($20)
+	sw $9 -6464($20)
+	
+	addi $25 $25 -1
+	addi $20 $20 -512
+	j castelo_topo_lateral
+	
+castelo_topo_lateral_esquerda_prep:
+	addi $25 $0 3
+	addi $20 $19 280
+	
+	ori $9 $0 0xA7D4B1 #verde claro
+	
+castelo_topo_lateral_esquerda:
+	beq $25 $0 voltar
+	
+	sw $9 -7184($20)
+	sw $9 -6672($20)
+	
+	
+	addi $25 $25 -1
+	addi $20 $20 -512
+	addi $20 $20 4
+	j castelo_topo_lateral_esquerda
